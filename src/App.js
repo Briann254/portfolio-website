e1a93fe9e5fab999483bbb87a97dbb5ec5ff096b
Briann254 <bnnderitu01@gmail.com>
2023-05-11 01:15:29 +0300

    Updated components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import './index.css';
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Project from "./routes/Project";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div >
-     
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="/about" element={ <About />} />
+        <Route path="/project" element={ <Project/>} />
+        <Route path="/contact" element={ <Contact />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./NavbarStyles.css";
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+        <Link to="/"> 
+        <h1>Portfolio</h1>
+        </Link>
+
+        <ul className='nav-menu'>
+            <li>
+                <Link to="/"> Home </Link>
+            </li>
+
+            <li>
+                <Link to="/project"> Project </Link>
+            </li>
+
+            <li>
+                <Link to="/about"> About </Link>
+            </li>
+        
+            <li>
+                <Link to="/contact"> Contact </Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
